9ffef4c5bdad9ba84afabd3fa83d3f60b38524d2
Mankirat <mankirat.bhullar23@gmail.com>
2025-01-16 15:50:18 +0530

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideStore()]
+  providers: [provideRouter(routes),
+  provideAnimationsAsync(),
+  provideStore(),
+  provideStoreDevtools(),
+  provideStore({})]
 };
